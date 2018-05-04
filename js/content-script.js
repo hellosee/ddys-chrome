98a3e10c8743c476b57aabb8381e5550d7e6eb94
hellosee <1335244575@qq.com>
2018-05-04 20:13:11 +0800

    h
    h

--- js/content-script.js
+++ js/content-script.js
@@ -1,1 +1,1 @@
-﻿console.log('这是content script!');
+﻿
--- js/popup.js
+++ js/popup.js
@@ -1,50 +1,48 @@
-/**
- * Created by lishoujie on 2018/5/3.
- */
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-    // Query filter to be passed to chrome.tabs.query - see
-    // https://developer.chrome.com/extensions/tabs#method-query
-    var queryInfo = {
-        active: true,
-        currentWindow: true
-    };
+$(function() {
+    $("#about").on("click",function(e){
+        window.open(chrome.extension.getURL('play.html'));
+    });
+    $('#donate').click(function(e){
+        window.open(chrome.extension.getURL('play.html'));
+    });
 
-    chrome.tabs.query(queryInfo, (tabs) => {
-        // chrome.tabs.query invokes the callback with a list of tabs that match the
-        // query. When the popup is opened, there is certainly a window and at least
-        // one tab, so we can safely assume that |tabs| is a non-empty array.
-        // A window can only have one active tab at a time, so the array consists of
-        // exactly one tab.
-        console.log(tabs);
-        var tab = tabs[0];
+    $('#playbtn').click(function(e){
+        window.open(chrome.extension.getURL('play.html'));
+    });
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
 });
-
-    // Most methods of the Chrome extension APIs are asynchronous. This means that
-    // you CANNOT do something like this:
-    //
-    // var url;
-    // chrome.tabs.query(queryInfo, (tabs) => {
-    //   url = tabs[0].url;
-    // });
-    // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+var parse = {
+    /**
+     * Get the current URL.
+     *
+     * @param {function(string)} callback called when the URL of the current tab
+     *   is found.
+     */
+    "getCurrentTabUrl" :function(callback){
+        // Query filter to be passed to chrome.tabs.query - see
+        // https://developer.chrome.com/extensions/tabs#method-query
+        var queryInfo = {
+          active:true,
+          currentWindow:true
+        };
+        chrome.tabs.query(queryInfo,function(tabs){
+            // chrome.tabs.query invokes the callback with a list of tabs that match the
+            // query. When the popup is opened, there is certainly a window and at least
+            // one tab, so we can safely assume that |tabs| is a non-empty array.
+            // A window can only have one active tab at a time, so the array consists of
+            // exactly one tab.
+            var tab = tabs[0];
+            // A tab is a plain object that provides information about the tab.
+            // See https://developer.chrome.com/extensions/tabs#type-Tab
+            var url = tab.url;
+            // tab.url is only available if the "activeTab" permission is declared.
+            // If you want to see the URL of other tabs (e.g. after removing active:true
+            // from |queryInfo|), then the "tabs" permission is required to see their
+            // "url" properties.
+            console.assert(typeof url == 'string', 'tab.url should be a string');
+            callback(url,tab.title);
+        });
+    }
 }
 
 // This extension loads the saved background color for the current tab if one
@@ -56,9 +54,36 @@ function getCurrentTabUrl(callback) {
 // chrome.storage.local allows the extension data to be synced across multiple
 // user devices.
 document.addEventListener('DOMContentLoaded', function() {
-    /*
-    getCurrentTabUrl((url) => {
+    parse.getCurrentTabUrl(function(url,title){
+        console.log(url);
+        console.log(title);
+        cHasMediaDiv(url,title);
+        /*
+        $.ajax({
+            url:"http://dw.local.com/vod/index",
+            type:'post',
+            async:true,
+            data:{'url':url},
+            dataType:'json',
+            beforeSend:function(){cLoadingDiv();},
+            success:function(data){
+                if(data.code == 1){
+                    var data = data.data;
+                    cHasMediaDiv(data.playUrl,data.title);
+                }
+            },
+            complete: function(){
+            }
+        });
 
-    });*/
-    console.log('hello chrome');
+
+        chrome.tabs.getSelected(null, function (tab) {　　// 先获取当前页面的tabID
+            chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(response) {
+                console.log(response);　　// 向content-script.js发送请求信息
+            });
+        });*/
+    });
 });
+
+
+
